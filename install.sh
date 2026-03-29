fcc19b3cdcf75ad3b824ca1e1d4831888557069c
Speedlight332 <130928690+Mohameda332@users.noreply.github.com>
2026-03-29 11:57:05 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -49,7 +49,7 @@ chmod u+rw $PREFIX/usr/etc/bash.bashrc
 echo "alias start=./startxfce4_debian.sh" | tee -a $PREFIX/usr/etc/bash.bashrc > /dev/null
 #Insall Pi-Apps & Firefox
 pd login debian --user "$username" -- env DISPLAY=:1.0 wget -qO- https://raw.githubusercontent.com/Botspot/pi-apps/master/install | bash
-pd login debian --user "$username" -- env DISPLAY=:1.0 sudo apt install firefox-esr
+pd login debian --user "$username" -- env DISPLAY=:1.0 sudo apt install firefox-esr -y
 echo "Restart Termux And Start XFCE4 with start command"
 rm install.sh 
 exit
